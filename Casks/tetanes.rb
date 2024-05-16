430e34292db4c8ba8223ccbc897647bed596f1b4
Lucas Petherbridge <lukexor@gmail.com>
2024-05-16 11:50:06 -0700

    Version bump: 0.10.0

---
+++ Casks/tetanes.rb
@@ -0,0 +1,15 @@
+cask "TetaNES" do
+  desc "A cross-platform NES emulator written in Rust using wgpu"
+  homepage "https://github.com/lukexor/tetanes"
+  version "0.10.0"
+  if Hardware::CPU.intel?
+    url "https://github.com/lukexor/tetanes/releases/download/tetanes-v0.10.0/tetanes-x86_64-apple.tar.gz"
+    sha256 "0f2920c1ee125872357d69f3cb4400796f2014a3dbb379c9d54b5f49cfc6ce8f"
+  else
+    url "https://github.com/lukexor/tetanes/releases/download/tetanes-v0.10.0/tetanes-aarch64-apple.tar.gz"
+    sha256 "9708c3cf28a5c635ea7fd50cfa128b4639748b2fcec55dc502869e8c471db83b"
+  end
+  license "MIT"
+
+  app "TetaNES.app"
+end
