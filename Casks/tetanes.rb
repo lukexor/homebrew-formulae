260b0384e690824065f57587cc3ab427941c58a9
Luke <lukexor@gmail.com>
2024-06-04 15:37:02 -0700

    fix: de-dupe version

--- Casks/tetanes.rb
+++ Casks/tetanes.rb
@@ -3,10 +3,10 @@ cask "tetanes" do
   homepage "https://github.com/lukexor/tetanes"
   version "0.10.0"
   if Hardware::CPU.intel?
-    url "https://github.com/lukexor/tetanes/releases/download/tetanes-v0.10.0/tetanes-0.10.0-x86_64-apple.tar.gz"
+    url "https://github.com/lukexor/tetanes/releases/download/tetanes-v#{version}/tetanes-#{version}-x86_64-apple.tar.gz"
     sha256 "0f2920c1ee125872357d69f3cb4400796f2014a3dbb379c9d54b5f49cfc6ce8f"
   else
-    url "https://github.com/lukexor/tetanes/releases/download/tetanes-v0.10.0/tetanes-0.10.0-aarch64-apple.tar.gz"
+    url "https://github.com/lukexor/tetanes/releases/download/tetanes-v#{version}/tetanes-#{version}-aarch64-apple.tar.gz"
     sha256 "9708c3cf28a5c635ea7fd50cfa128b4639748b2fcec55dc502869e8c471db83b"
   end
   license "MIT"
