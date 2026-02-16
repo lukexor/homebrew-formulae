549bf30a93c0fbeb0dfe470b173e0555f27466ba
Luke Petherbridge <lukexor@gmail.com>
2026-02-16 09:07:47 -0800

    ci: fix ci. closes #1

--- Casks/tetanes.rb
+++ Casks/tetanes.rb
@@ -1,15 +1,22 @@
 cask "tetanes" do
-  desc "A cross-platform NES emulator written in Rust using wgpu"
-  homepage "https://github.com/lukexor/tetanes"
+  arch arm: "arm64", intel: "x86_64"
+
   version "0.13.0"
-  if Hardware::CPU.intel?
-    url "https://github.com/lukexor/tetanes/releases/download/tetanes-v#{version}/tetanes-#{version}-x86_64-apple.tar.gz"
-    sha256 "b5c2152a29f46bd97623bc45c0a54b8a71c8b88886cbe4d774276a9517337bd4"
-  else
-    url "https://github.com/lukexor/tetanes/releases/download/tetanes-v#{version}/tetanes-#{version}-aarch64-apple.tar.gz"
+
+  on_arm do
     sha256 "75c3b400200464276706a10152dfe66928582ae282af09fd2c363fe612cb3d71"
+
+    url "https://github.com/lukexor/tetanes/releases/download/tetanes-v#{version}/tetanes-#{version}-aarch64-apple.tar.gz"
   end
-  license "MIT"
+  on_intel do
+    sha256 "b5c2152a29f46bd97623bc45c0a54b8a71c8b88886cbe4d774276a9517337bd4"
+
+    url "https://github.com/lukexor/tetanes/releases/download/tetanes-v#{version}/tetanes-#{version}-x86_64-apple.tar.gz"
+  end
+
+  name "TetaNES"
+  desc "Cross-platform NES emulator written in Rust using wgpu"
+  homepage "https://github.com/lukexor/tetanes"
 
   app "TetaNES.app"
 end
