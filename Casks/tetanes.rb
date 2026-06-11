637e6a0f45c3ac9f1cf4ad5816fd3a1417df0204
lukexor <709609+lukexor@users.noreply.github.com>
2026-06-11 13:53:14 +0000

    Version Bump vtetanes-core-v0.14.2

--- Casks/tetanes.rb
+++ Casks/tetanes.rb
@@ -1,15 +1,15 @@
 cask "tetanes" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.14.1"
+  version "tetanes-core-v0.14.2"
 
   on_arm do
-    sha256 "1641d10eb2c90ec42ff63dfee412b0f1ed401c4550fcd0a9118b1bca2c1aeacc"
+    sha256 "2da852b40e9bf9e8b21cb1db2597a5cbdebb9653cd38a484aa2d8f26e684cb96"
 
     url "https://github.com/lukexor/tetanes/releases/download/tetanes-v#{version}/tetanes-#{version}-aarch64-apple.tar.gz"
   end
   on_intel do
-    sha256 "77ca7696bc937eb93ea94a1fdb0ef148be991e2fa6e3f2f7dad835fbde90bb35"
+    sha256 "bffcafbc9467a1ecae420b5fafee942ad3c9afcb084caeca99cc60adb50f3b89"
 
     url "https://github.com/lukexor/tetanes/releases/download/tetanes-v#{version}/tetanes-#{version}-x86_64-apple.tar.gz"
   end
