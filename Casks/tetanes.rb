7d873e146a43401236fea8b01f33c651bdb8e97d
Luke Petherbridge <lukexor@gmail.com>
2026-08-18 20:22:01 -0700

    lint: fixed stanza grouping

--- Casks/tetanes.rb
+++ Casks/tetanes.rb
@@ -5,10 +5,12 @@ cask "tetanes" do
 
   on_arm do
     sha256 "32ace03f5cb1ceb1582d53166b9267c2b90d528b89747f10abd8d01d15e761f3"
+
     url "https://github.com/lukexor/tetanes/releases/download/tetanes-v#{version}/tetanes-#{version}-aarch64-apple.tar.gz"
   end
   on_intel do
     sha256 "3c82bc4c13764539911965d0350602041fe0a6d0e0f296ddfbf06b584fda253c"
+
     url "https://github.com/lukexor/tetanes/releases/download/tetanes-v#{version}/tetanes-#{version}-x86_64-apple.tar.gz"
   end
 
